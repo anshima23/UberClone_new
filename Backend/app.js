086f1d79fa7484b97a29c2c8edf59131a4bdda60
Anshima <anshima541@gmail.com>
2025-01-14 10:59:44 +0530

    service

--- Backend/app.js
+++ Backend/app.js
@@ -3,10 +3,22 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+const CaptainRoutes = require('./routes/captain.routes');
+
+connectToDb();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     res.send('Hello world');
 });
+
+app.use('./users',userRoutes);
+app.use('./captain',CaptainRoutes);
 
 module.exports = app;
